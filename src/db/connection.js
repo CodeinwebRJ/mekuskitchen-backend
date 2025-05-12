fbeedab63d23f4833d2dc5ac94122e35b9d4c4e9
MEETSIDHDHAPURA <meet.mbinfoways@gmail.com>
2025-05-12 15:48:09 +0530

    change in res

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,20 +3,18 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   if (!process.env.MONGODB_URI) {
-    return res
-      .status(400)
-      .json(new Error("MONGODB_URI is not defined in environment variables"));
+    throw new Error("MONGODB_URI is not defined in environment variables");
   }
 
   try {
     const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
     console.log(
-      `\n MongoDB connected! Host: ${connectionInstance.connection.host}`
+      `\nMongoDB connected! Host: ${connectionInstance.connection.host}`
     );
     return connectionInstance;
   } catch (error) {
     console.error("MongoDB connection failed:", error);
-    return res.status(400).json(error); 
+    throw error;
   }
 };
 
