4c5776f00b6819b17f08bc1c657fe8a2247c61cd
Your Name <mehulrabari2005@gmail.com>
2025-08-29 19:14:47 +0530

    feat:create new end-point in the controller TifinPincodeMaster

--- src/routes/TiffinPincodeMaster.route.js
+++ src/routes/TiffinPincodeMaster.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getPincodeList, CreatePincode, UpdatePincode, DeletePincode } = require("../controllers/TiffinPincodeMaster");
+const { getPincodeList, CreatePincode, UpdatePincode, DeletePincode, CheckPincode } = require("../controllers/TiffinPincodeMaster");
 
 const PincodeMasterRoute = express.Router();
 
@@ -7,5 +7,6 @@ PincodeMasterRoute.get("/get/pincode",getPincodeList);
 PincodeMasterRoute.post("/add/pincode",CreatePincode);
 PincodeMasterRoute.patch("/update/pincode",UpdatePincode);
 PincodeMasterRoute.delete("/delete/pincode",DeletePincode);
+PincodeMasterRoute.get("/check/pincode/:code", CheckPincode);
 
 module.exports = PincodeMasterRoute;
